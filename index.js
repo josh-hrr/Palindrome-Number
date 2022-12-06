d6fd271cf50ed080eaa824c635f9086c593382af
Josué Herrera <54568984+josh-hrr@users.noreply.github.com>
2022-12-06 05:03:34 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,17 @@
+let isPalindrome = function(x) { 
+    let newXArray = `${x}`.split(''); 
+    let newXReverse = newXArray.reverse();
+    let newXReverseJoin = newXReverse.join('');
+
+    if(x == newXReverseJoin){
+        return true;
+    }else{
+        return false;
+    }
+
+};
+
+
+let x = -121;
+
+console.log(isPalindrome(x));
